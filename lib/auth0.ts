74c41baa277991059a46e4b174ae09f137f0f75e
David Damilola <37887398+dagbolade@users.noreply.github.com>
2023-03-26 20:32:38 +0100

    ...

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,13 +1,13 @@
 import { ManagementClient, AuthenticationClient } from 'auth0';
 
 const auth0 = new AuthenticationClient({
-  domain: process.env.AUTH0_DOMAIN!,
+  domain: process.env.AUTH0_BASE_URL!,
   clientId: process.env.AUTH0_CLIENT_ID!,
   clientSecret: process.env.AUTH0_CLIENT_SECRET!,
 });
 
 const auth0Management = new ManagementClient({
-  domain: process.env.AUTH0_DOMAIN!,
+  domain: process.env.AUTH0_BASE_URL!,
   clientId: process.env.AUTH0_CLIENT_ID!,
   clientSecret: process.env.AUTH0_CLIENT_SECRET!,
   scope: 'read:users update:users',
